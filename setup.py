ac30a4874845cfb0566fe76701aa1f65b4ba9c35
Steven Silvester <steven.silvester@ieee.org>
2022-11-04 14:31:14 -0500

    update supported python versions

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     keywords=["mongo", "mongodb", "pymongo-auth-aws", "pymongo", "MONGODB-AWS"],
     test_suite="test",
     license="Apache License, Version 2.0",
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
