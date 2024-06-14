69ae1040185bb75ad93d37c6f4638d1c77ae3e3d
Steven Silvester <steve.silvester@mongodb.com>
2024-06-14 07:26:27 -0500

    fix assign-pr-reviewer

--- .evergreen/assign-pr-reviewer.sh
+++ .evergreen/assign-pr-reviewer.sh
@@ -4,5 +4,5 @@ set -x
 export CONFIG=$PROJECT_DIRECTORY/.github/reviewers.txt
 export SCRIPT="$DRIVERS_TOOLS/.evergreen/github_app/assign-reviewer.sh"
 # shellcheck disable=SC2154
-bash $SCRIPT -p $CONFIG -h ${github_commit} -o "mongodb" -n "winkerberos"
+bash $SCRIPT -p $CONFIG -h ${github_commit} -o "mongodb" -n "pymongo-auth-aws"
 echo '{"results": [{ "status": "PASS", "test_file": "Build", "log_raw": "Test completed"  } ]}' > ${PROJECT_DIRECTORY}/test-results.json
