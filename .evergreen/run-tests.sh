b978fcfab7ac3b320d493dc96faa8492968b636a
Steven Silvester <steve.silvester@mongodb.com>
2025-03-06 19:59:59 -0600

    PYTHON-5190 Update integration script

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -3,10 +3,6 @@
 set +x
 set -eu
 
-# Fetch secrets
-bash ${DRIVERS_TOOLS}/.evergreen/auth_aws/setup-secrets.sh
-source ${DRIVERS_TOOLS}/.evergreen/auth_aws/secrets-export.sh
-
 # Set up env
 pushd ..
 git clone https://github.com/mongodb/mongo-python-driver
@@ -19,6 +15,10 @@ pip install "./mongo-python-driver[test]"
 pip install -e ./src
 
 pushd ./mongo-python-driver
-.evergreen/scripts/run-mongodb-aws-test.sh regular
+bash ./.evergreen/just.sh run-server auth_aws
+bash ./.evergreen/just.sh setup-tests auth_aws regular
+source ./.evergreen/scripts/test-env.sh
+python -m pytest -v -m auth_aws
+
 popd
 popd
